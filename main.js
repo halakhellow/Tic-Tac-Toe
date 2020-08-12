900941bfde804e54e1d1e6ea36e39617ceeb51a4
Hala Alkhellow <halakhellow1996@gmail.com>
2020-08-12 03:01:50 +0300

    add main functions

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 "use strict"
+let reset = document.getElementById("reset-btn");
+let turn = "X";
+let cells = Array.from(document.querySelectorAll(".cell"));
+
+function isCellEmpty(cell) {
+    if (cell.innerHTML == "") return true;
+    else return false;
+}
+
+function addMove(cell) {
+    let index = cell.target.id.replace("cell", "");
+    if (isCellEmpty(cells[index])) {
+        cells[index].innerHTML = turn;
+        cells[index].classList.add("filled");
+        if (turn == "X") turn = "O";
+        else if (turn == "O") turn = "X";
+    }
+}
+
+cells.forEach((cell) => cell.addEventListener('click', addMove));
+
+function CheckWinner() {
+
+}
+
+reset.onclick = function () {
+    for (let i = 0; i < 9; i++) {
+        cells[i].innerHTML = "";
+        cells[i].classList.remove("filled");
+    }
+}
